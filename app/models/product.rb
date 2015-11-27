7111784b6b4ded52b33e3234372b5e26ee9e80af
Rózsa Dániel <rozsa.daniel@ehok.u-szeged.hu>
2015-11-27 15:34:25 +0100

    Welcome sceen with live data without image

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,8 +5,11 @@ class Product < ActiveRecord::Base
 
   has_many :reviews
 
-  validates :product_picture, presence: true
+#  validates :product_picture, presence: true
    has_attached_file :product_picture, styles: { main: '2000x750', medium: '650x350', thumb: "300x400>"  }
    validates_attachment_content_type :product_picture, content_type: /\Aimage\/.*\Z/
 
+   def price_with_tag
+     "#{price} HUF"
+   end
 end
